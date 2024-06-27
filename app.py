b7dc5d474af3d054ec0fdebf8ff3b47b63666ddc
Shalin Ahasan <shalin.a.ahasan@gmail.com>
2024-06-27 12:46:27 +0100

    update

--- app.py
+++ app.py
@@ -75,4 +75,5 @@ def get_url():
         return jsonify({"error": f"No URL found for shortener '{shortener}'"}), 404
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=True, host='0.0.0.0', port=port)
