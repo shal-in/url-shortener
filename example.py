1c6c5e35d748f330e6e0d35b7ec7c886f12e5808
Shalin Ahasan <shalin.a.ahasan@gmail.com>
2024-06-27 13:14:45 +0100

    example

--- example.py
+++ example.py
@@ -1,16 +1,19 @@
 from flask import Flask, jsonify
+import json
 import os
 
 app = Flask(__name__)
 app.secret_key = "secretkey12"
 
-key_json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+GOOGLE_APPLICATION_CREDENTIALS_JSON = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+GOOGLE_APPLICATION_CREDENTIALS = json.loads(GOOGLE_APPLICATION_CREDENTIALS_JSON)
 EXPECTED_PASSWORD = os.getenv("EXPECTED_PASSWORD")
 
 @app.route("/")
 def index():
-    data = {"key_json": key_json, "expected_password": EXPECTED_PASSWORD}
+    data = {"key_json": GOOGLE_APPLICATION_CREDENTIALS, "expected_password": EXPECTED_PASSWORD}
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=True, host='0.0.0.0', port=port)
